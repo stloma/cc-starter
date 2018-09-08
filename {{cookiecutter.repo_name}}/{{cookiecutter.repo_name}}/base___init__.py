a57f5ed9935c2b1bc45bbacb9f453d2ad0691d8f
Stephen Martin <lockwood@opperline.com>
2018-09-08 04:45:02 -0700

    add context manager

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/base___init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/base___init__.py
@@ -13,20 +13,20 @@ def root_factory(request):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
-{%- if cookiecutter.backend == 'sqlalchemy' %}
-    config.include('.models')
-{%- endif %}
-{%- if cookiecutter.backend == 'zodb' %}
-    settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
-    config.include('pyramid_tm')
-    config.include('pyramid_retry')
-    config.include('pyramid_zodbconn')
-    config.set_root_factory(root_factory)
-{%- else %}
-    config.add_route('home', '/')
-{%- endif %}
-    config.include('pyramid_{{ cookiecutter.template_language }}')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.scan()
-    return config.make_wsgi_app()
+    with Configurator(settings=settings) as config:
+    {%- if cookiecutter.backend == 'sqlalchemy' %}
+        config.include('.models')
+    {%- endif %}
+    {%- if cookiecutter.backend == 'zodb' %}
+        settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
+        config.include('pyramid_tm')
+        config.include('pyramid_retry')
+        config.include('pyramid_zodbconn')
+        config.set_root_factory(root_factory)
+    {%- else %}
+        config.add_route('home', '/')
+    {%- endif %}
+        config.include('pyramid_{{ cookiecutter.template_language }}')
+        config.add_static_view('static', 'static', cache_max_age=3600)
+        config.scan()
+        return config.make_wsgi_app()
